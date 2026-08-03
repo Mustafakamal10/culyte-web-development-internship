e48e7cf9f54d581809c9b0ac2b9a8969787b7535
mustafakamal10 <mustafakamal10@example.com>
2026-08-03 23:24:44 +0500

    Add Week 4 project

---
+++ Week-4/src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+
+// React is a JavaScript library used to build user interfaces.
+
+
+// Function Component
+
+function App() {
+
+    // State stores data that can change.
+
+    const [count, setCount] = useState(0);
+
+    const [name, setName] = useState("Mustafa");
+
+
+    // Increase Count
+
+    function increase() {
+        setCount(count + 1);
+    }
+
+
+    // Change Name
+
+    function changeName() {
+        setName("Ali");
+    }
+
+
+    return (
+
+        // JSX must return one parent element.
+
+        <div>
+
+            <h1>React Basics</h1>
+
+            {/* JavaScript is written inside {} */}
+
+            <h2>Hello {name}</h2>
+
+            <p>Count : {count}</p>
+
+            {/* onClick calls a function */}
+
+            <button onClick={increase}>Increase</button>
+
+            <button onClick={changeName}>Change Name</button>
+
+            {/* Reusable Component */}
+
+            <Student
+                studentName="Ahmed"
+                semester="Final Year"
+            />
+
+            <Student
+                studentName="Mustafakamal"
+                semester="7th Semester"
+            />
+
+        </div>
+
+    );
+
+}
+
+
+// Props receive data from the parent component.
+
+function Student(props) {
+
+    return (
+
+        <div>
+
+            <h3>{props.studentName}</h3>
+
+            <p>{props.semester}</p>
+
+        </div>
+
+    );
+
+}
+
+
+export default App;
